1a4ec9f4b05d7f1e31c24b1cfb843edd29f01a4b
Vedant Nevetia <vnevetia@redhat.com>
2021-09-18 18:30:42 -0500

    Initial commit

---
+++ src/main/java/com/covnev/jounral/JounralApplication.java
@@ -0,0 +1,13 @@
+package com.covnev.jounral;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JounralApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JounralApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/covnev/jounral/JounralApplicationTests.java
@@ -0,0 +1,13 @@
+package com.covnev.jounral;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JounralApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
